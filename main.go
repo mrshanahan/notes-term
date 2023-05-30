7531d28a6dfabe97606d5220913eeba10891a1c9
Matt Shanahan <mrshanahan11235@gmail.com>
2023-05-29 21:28:26 -0400

    Refactor to draw window on every operation

--- main.go
+++ main.go
@@ -23,8 +23,34 @@ const (
     DEFAULT_FOREGROUND_COLOR = 37 // white
     HIGHLIGHT_BACKGROUND_COLOR = 47 // white
     HIGHLIGHT_FOREGROUND_COLOR = 36 // cyan
-
 )
+
+type Palette struct {
+    Background int
+    Foreground int
+}
+
+type Window struct {
+    Selection int
+    Notes []string
+    Width int
+    Height int
+}
+
+var (
+    DefaultPalette = &Palette{
+        DEFAULT_BACKGROUND_COLOR,
+        DEFAULT_FOREGROUND_COLOR,
+    }
+    HighlightPalette = &Palette{
+        HIGHLIGHT_BACKGROUND_COLOR,
+        HIGHLIGHT_FOREGROUND_COLOR,
+    }
+)
+
+func (w *Window) GetTextBounds() (int, int, int, int) {
+    return 2, w.Height-1, 2, w.Width-1
+}
 
 func Move(row, col int) {
     fmt.Printf("\033[%d;%dH", row, col)
@@ -42,35 +68,12 @@ func ClearScreen() {
     fmt.Printf("\033[2J")
 }
 
-func HighlightRow(contents string, row, rowmin, colmin, colmax int) {
-    Move(row, colmin)
-    fmt.Printf("\033[%dm", HIGHLIGHT_BACKGROUND_COLOR)
-    fmt.Printf("\033[%dm", HIGHLIGHT_FOREGROUND_COLOR)
-
-    padding := colmax - rowmin - len(contents)
-    if padding < 0 {
-        padding = 0
-        contents = contents[:colmax-rowmin-2] + "..."
-    }
-    padstring := strings.Repeat(" ", padding)
-    fmt.Printf("%s%s", contents, padstring)
-
-    fmt.Printf("\033[%dm", DEFAULT_BACKGROUND_COLOR)
-    fmt.Printf("\033[%dm", DEFAULT_FOREGROUND_COLOR)
+func HideCursor() {
+    fmt.Print("\033[?25l")
 }
 
-func ResetRow(contents string, row, rowmin, colmin, colmax int) {
-    Move(row, colmin)
-    fmt.Printf("\033[%dm", DEFAULT_BACKGROUND_COLOR)
-    fmt.Printf("\033[%dm", DEFAULT_FOREGROUND_COLOR)
-
-    padding := colmax - rowmin - len(contents)
-    if padding < 0 {
-        padding = 0
-        contents = contents[:colmax-rowmin-2] + "..."
-    }
-    padstring := strings.Repeat(" ", padding)
-    fmt.Printf("%s%s", contents, padstring)
+func ShowCursor() {
+    fmt.Print("\033[?25h")
 }
 
 func DrawChar(r, c int, b int) {
@@ -103,6 +106,44 @@ func DrawInterior(rowmin, rowmax, colmin, colmax int) {
     }
 }
 
+func DrawNoteRow(window *Window, noteIdx int, palette *Palette) {
+    rowmin, _, colmin, colmax := window.GetTextBounds()
+    if noteIdx < 0 || noteIdx >= len(window.Notes) {
+        panic(fmt.Sprintf("attempted to draw nonexistent note index: %d", noteIdx))
+    }
+
+    row, col := noteIdx + rowmin, colmin
+    Move(row, col)
+    fmt.Printf("\033[%dm", palette.Background)
+    fmt.Printf("\033[%dm", palette.Foreground)
+
+    contents := window.Notes[noteIdx]
+    padding := colmax - rowmin - len(contents) + 1
+    if padding < 0 {
+        padding = 0
+        contents = contents[:colmax-rowmin-2] + "..."
+    }
+    padstring := strings.Repeat(" ", padding)
+    fmt.Printf("%s%s", contents, padstring)
+
+    fmt.Printf("\033[%dm", DefaultPalette.Background)
+    fmt.Printf("\033[%dm", DefaultPalette.Foreground)
+}
+
+func Draw(window *Window) {
+    rowmin, rowmax, colmin, colmax := window.GetTextBounds()
+    DrawBorders(rowmin, rowmax, colmin, colmax)
+    DrawInterior(rowmin, rowmax, colmin, colmax)
+
+    for i, _ := range window.Notes {
+        if i == window.Selection {
+            DrawNoteRow(window, i, HighlightPalette)
+        } else {
+            DrawNoteRow(window, i, DefaultPalette)
+        }
+    }
+}
+
 func LoadIndex(path string) []string {
     f, err := os.Open(path)
     if err != nil {
@@ -122,10 +163,7 @@ func LoadIndex(path string) []string {
     return lines
 }
 
-func main() {
-    notes := LoadIndex("index.txt")
-
-    fd := os.Stdin.Fd()
+func DisableEcho(fd uintptr) {
     t := &unix.Termios{}
     err := termios.Tcgetattr(fd, t)
     if err != nil {
@@ -137,6 +175,13 @@ func main() {
     if err != nil {
         panic(err)
     }
+}
+
+func main() {
+    notes := LoadIndex("index.txt")
+
+    fd := os.Stdin.Fd()
+    DisableEcho(fd)
     // t.Lflag = t.Lflag | unix.ECHO
     // defer termios.Tcsetattr(fd, termios.TCSANOW, t)
 
@@ -153,65 +198,38 @@ func main() {
         panic(err)
     }
 
-    fmt.Print("\033[?25l")
-    defer fmt.Print("\033[?25h")
+    HideCursor()
+    defer ShowCursor()
 
     SetBackgroundColor()
     defer ResetBackgroundColor()
 
-    rowmin, rowmax, colmin, colmax := 2, termh-1, 2, termw-1
-    DrawBorders(rowmin, rowmax, colmin, colmax)
-    DrawInterior(rowmin, rowmax, colmin, colmax)
-
-    for i, note := range notes {
-        Move(rowmin+i, colmin)
-        fmt.Print(note)
-    }
-
-    lastrow := rowmin + len(notes) - 1
-
-    row, col := rowmin, colmin 
-    HighlightRow(notes[row-rowmin], row, rowmin, colmin, colmax)
-    Move(row, col)
+    window := &Window{0, notes, termw, termh}
+    Draw(window)
 
     for c != 'q' {
         // TODO: interrupts
         os.Stdin.Read(b)
         c = b[0]
-        prevrow := row
+        idx := window.Selection
         switch (c) {
         case 'k':
-            if (row <= rowmin) {
-                row = rowmin
+            if (idx <= 0) {
+                idx = 0
             } else {
-                row -= 1
+                idx -= 1
             }
             break
         case 'j':
-            if (row >= lastrow) {
-                row = lastrow
+            if (idx >= len(window.Notes)-1) {
+                idx = len(window.Notes)-1
             } else {
-                row += 1
+                idx += 1
             }
             break
-        // case 'h':
-        //     if (col <= colmin) {
-        //         col = colmin
-        //     } else {
-        //         col -= 1
-        //     }
-        //     break
-        // case 'l':
-        //     if (col >= colmax) {
-        //         col = colmax 
-        //     } else {
-        //         col += 1
-        //     }
-        //     break
         }
-        ResetRow(notes[prevrow-rowmin], prevrow, rowmin, colmin, colmax)
-        HighlightRow(notes[row-rowmin], row, rowmin, colmin, colmax)
-        Move(row, col)
+        window.Selection = idx
+        Draw(window)
     }
 
     Move(0,0)
